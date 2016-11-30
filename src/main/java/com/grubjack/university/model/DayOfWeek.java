3647f181d2d31d82ea3ae5067345cbd46e62b2dd
Aleksandr Urban <grubjack@gmail.com>
2016-11-30 14:52:05 +0200

    toString method

--- src/main/java/com/grubjack/university/model/DayOfWeek.java
+++ src/main/java/com/grubjack/university/model/DayOfWeek.java
@@ -22,4 +22,9 @@ public enum DayOfWeek {
         }
         return names;
     }
+
+    @Override
+    public String toString() {
+        return name().substring(0, 1).toUpperCase() + name().substring(1).toLowerCase();
+    }
 }
